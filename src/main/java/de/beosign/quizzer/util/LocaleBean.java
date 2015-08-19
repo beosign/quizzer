522a4e3aca185350bfc785803729505a8209ca46
Florian Dahlmanns <florian.dahlmanns@freenet.de>
2015-08-19 16:36:20 +0200

    Localization support

---
+++ src/main/java/de/beosign/quizzer/util/LocaleBean.java
@@ -0,0 +1,36 @@
+package de.beosign.quizzer.util;
+
+import java.io.Serializable;
+import java.util.Locale;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.apache.logging.log4j.Logger;
+
+@SessionScoped
+@Named
+public class LocaleBean implements Serializable {
+    private static final long serialVersionUID = 6480930477147438916L;
+
+    @Inject
+    private Logger logger;
+
+    private Locale locale;
+
+    public void changeLocale(String locale) {
+        logger.debug("Changing locale to {}", locale);
+        this.locale = new Locale(locale);
+    }
+
+    public Locale getLocale() {
+        logger.debug("locale: {}", locale);
+        return locale;
+    }
+
+    public void setLocale(Locale locale) {
+        logger.debug("new locale: {}", locale);
+        this.locale = locale;
+    }
+}
