09053d171e899e9b608a8110e37de38961a59291
Florian Dahlmanns <florian.dahlmanns@freenet.de>
2015-08-19 17:14:27 +0200

    Bugfix in DurationConverter

--- src/main/java/de/beosign/quizzer/converter/DurationMinutesConverter.java
+++ src/main/java/de/beosign/quizzer/converter/DurationMinutesConverter.java
@@ -3,10 +3,12 @@ package de.beosign.quizzer.converter;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
+import javax.inject.Named;
 
 import org.apache.logging.log4j.Logger;
 import org.primefaces.model.DualListModel;
@@ -21,7 +23,9 @@ import de.beosign.quizzer.model.Question;
  * @author Florian Dahlmanns
  *
  */
-@FacesConverter(value = DurationMinutesConverter.DURATION_MINUTES_CONVERTER)
+@ApplicationScoped
+@Named
+@FacesConverter(DurationMinutesConverter.DURATION_MINUTES_CONVERTER)
 public class DurationMinutesConverter implements Converter {
     public static final String DURATION_MINUTES_CONVERTER = "DurationMinutesConverter";
 
@@ -42,8 +46,23 @@ public class DurationMinutesConverter implements Converter {
     @Override
     public String getAsString(FacesContext context, UIComponent component, Object value) {
         LOGGER.debug("component: {}, value: {}", component.getClass().getName(), value);
-        Duration duration = (Duration) value;
+
+        if (value == null) {
+            return "0";
+        }
+
+        Duration duration = Duration.ofMinutes(0);
+        if (value instanceof Duration) {
+
+            duration = (Duration) value;
+        } else {
+            throw new IllegalArgumentException("The value to convert is not of type Duration, but: " + value.getClass().getName());
+        }
         return String.valueOf(duration.get(ChronoUnit.SECONDS) / 60);
+    }
+
+    public String getConverterId() {
+        return DURATION_MINUTES_CONVERTER;
     }
 
 }
